4a3f5d8aa70738c5d24a868daec8e8b374dfc3a4
Chris Perko <Chris@chrisperko.net>
2017-10-07 11:58:29 -0500

    Prevent duplicate email signups

--- app/Http/Controllers/WelcomePageEmailSignupController.php
+++ app/Http/Controllers/WelcomePageEmailSignupController.php
@@ -10,6 +10,10 @@ class WelcomePageEmailSignupController extends Controller
     public function signup(Request $request) {
     	$email = $request->get('email');
 
+    	$exists = WelcomeEmailAddress::where('email', $email)->count();
+
+    	if($exists > 0) return false;
+
     	WelcomeEmailAddress::create(['email' => $email]);
     }
 }
--- tests/Feature/WelcomePageEmailSignupTest.php
+++ tests/Feature/WelcomePageEmailSignupTest.php
@@ -6,17 +6,25 @@ use App\WelcomeEmailAddress;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class WelcomePageEmailSignupTest extends TestCase
-{
+class WelcomePageEmailSignupTest extends TestCase {
 	use RefreshDatabase;
 
-    /** @test */
-    public function it_can_register_an_email_address()
-    {
-        $this->post('/remindme', ['email' => 'testemail@test.com']);
+	/** @test */
+	public function it_can_register_an_email_address() {
+		$this->post( '/remindme', [ 'email' => 'testemail@test.com' ] );
 
-        $count = WelcomeEmailAddress::where('email', 'testemail@test.com')->count();
+		$count = WelcomeEmailAddress::where( 'email', 'testemail@test.com' )->count();
 
-        $this->assertEquals(1, $count);
-    }
+		$this->assertEquals( 1, $count );
+	}
+
+	/** @test */
+	public function it_will_not_create_duplicates() {
+		$this->post( '/remindme', [ 'email' => 'testemail@test.com' ] );
+		$this->post( '/remindme', [ 'email' => 'testemail@test.com' ] );
+
+		$count = WelcomeEmailAddress::where( 'email', 'testemail@test.com' )->count();
+
+		$this->assertEquals( 1, $count );
+	}
 }
