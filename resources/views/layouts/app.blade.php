51da62fc74d6c433d7ac650e879fc0e01b20e4fa
Chris Perko <Chris@chrisperko.net>
2017-10-07 12:15:12 -0500

    Google Analytics tracking

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,4 +54,16 @@
 <!-- JS -->
 <script src="{{asset('js/app.js')}}"></script>
 
+@if(env('APP_ENV') != 'local')
+<!-- Global Site Tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-26651291-14"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-26651291-14');
+</script>
+@endif
+
 </html>
