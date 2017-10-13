2891dcd5ffdc7e15d73e566c5a9552847516b177
Chris Perko <christopher.j.perko@windstream.com>
2017-10-13 10:11:32 -0500

    Added back the fields test

---
+++ tests/Feature/CustomizerFieldTest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\CustomizerField;
+use App\Section;
+use App\Theme;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class CustomizerFieldTest extends TestCase {
+	use DatabaseTransactions;
+
+	/** @test */
+	public function it_has_a_page_to_add_the_fields() {
+		$section = factory( Section::class )->create();
+
+		$this->get( "/theme/$section->theme_id/sections/$section->id/fields" )->assertStatus( 200 );
+	}
+
+	/** @test */
+	public function it_will_not_allow_anonymous_users_to_view_a_users_fields() {
+		$user    = factory( User::class )->create();
+		$theme   = factory( Theme::class )->create( [ 'user_id' => $user->id ] );
+		$section = factory( Section::class )->create( [ 'theme_id' => $theme->id ] );
+
+		$this->get( "/theme/$theme->id/sections/$section->id/fields" )->assertStatus( 302 );
+	}
+
+	/** @test */
+	public function it_allows_a_user_to_see_their_own_fields() {
+		$user    = factory( User::class )->create();
+		$theme   = factory( Theme::class )->create( [ 'user_id' => $user->id ] );
+		$section = factory( Section::class )->create( [ 'theme_id' => $theme->id ] );
+
+		$this->actingAs( $user )->get( "/theme/$theme->id/sections/$section->id/fields" )->assertStatus( 200 );
+	}
+
+	/** @test */
+	public function it_will_not_allow_a_signed_in_user_to_view_another_users_fields() {
+		$user    = factory( User::class )->create();
+		$theme   = factory( Theme::class )->create( [ 'user_id' => $user->id ] );
+		$section = factory( Section::class )->create( [ 'theme_id' => $theme->id ] );
+
+		$anotherUser = factory( User::class )->create();
+		$this->actingAs( $anotherUser )->get( "/theme/$theme->id/sections/$section->id/fields" )->assertStatus( 302 );
+	}
+
+	/** @test */
+	public function it_can_create_a_new_field() {
+		$section = factory( Section::class )->create();
+
+		$this->post( "/theme/$section->theme_id/sections/$section->id/fields", [
+			"label"   => "My Field",
+			"default" => "#00ff00",
+		] )->assertStatus( 200 );
+
+		$savedField = CustomizerField::Section( $section->id )->first();
+
+		$this->assertEquals( "My Field", $savedField->label );
+		$this->assertEquals( "#00ff00", $savedField->default );
+	}
+
+	/** @test */
+	public function it_wont_let_anonymous_users_add_to_other_users_class() {
+		$user    = factory( User::class )->create();
+		$theme   = factory( Theme::class )->create( [ 'user_id' => $user->id ] );
+		$section = factory( Section::class )->create( [ 'theme_id' => $theme->id ] );
+
+		$this->post( "/theme/$section->theme_id/sections/$section->id/fields", [
+			"label"      => "My Field",
+			"default"    => "#00ff00",
+			"section_id" => $section->id,
+		] )->assertStatus( 302 );
+	}
+
+	/** @test */
+	public function it_wont_allow_users_to_create_on_other_users_class() {
+		$user    = factory( User::class )->create();
+		$theme   = factory( Theme::class )->create( [ 'user_id' => $user->id ] );
+		$section = factory( Section::class )->create();
+
+		$anotherUser = factory( User::class )->create();
+		$this->actingAs( $anotherUser )->post( "/theme/$theme->id/sections/$section->id/fields", [
+			"label"      => "My Field",
+			"default"    => "#00ff00",
+			"section_id" => $section->id,
+		] )->assertStatus( 302 );
+
+		$this->actingAs( $user )->post( "/theme/$theme->id/sections/$section->id/fields", [
+			"label"      => "My Field",
+			"default"    => "#00ff00",
+			"section_id" => $section->id,
+		] )->assertStatus( 200 );
+	}
+}
