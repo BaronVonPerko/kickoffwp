2c190a9d0bd85848e330e8b3d2a97c0aa828dd54
Chris Perko <Chris@chrisperko.net>
2017-10-14 12:08:34 -0500

    Frontend work for editing a theme name

--- resources/assets/js/components/ThemeCard.vue
+++ resources/assets/js/components/ThemeCard.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="card green lighten-3 z-depth-3" v-if="!isDeleted">
         <div class="card-content black-text">
-            <span class="card-title">{{theme.name}}</span>
+            <span class="card-title">
+                <span v-if="!editing">{{theme.name}}</span>
+                <form v-if="editing" @submit.prevent="">
+                    <input type="text" v-if="editing" v-model="theme.name">
+                    <button @click="saveChanges()" class="btn">Save Changes</button>
+                </form>
+            </span>
             <ul class="row">
                 <li class="col s6" v-for="section in theme.sections">
                     <a :href="sectionLink(section)">
@@ -12,11 +18,12 @@
             </ul>
         </div>
         <div class="card-action">
-            <!--<theme-card-buttons :theme="theme" :mark-deleted="markDeleted"></theme-card-buttons>-->
-            <a :href="sectionsLink" class="btn">Sections</a>
-            <a href="#" class="btn-floating"><i class="material-icons">edit</i></a>
-            <a href="#" class="btn-floating"><i class="material-icons">content_copy</i></a>
-            <a @click="deleteTheme()" class="btn-floating"><i class="material-icons">delete</i></a>
+            <div v-if="!editing">
+                <a :href="sectionsLink" class="btn">Sections</a>
+                <a @click="edit()" class="btn-floating"><i class="material-icons">edit</i></a>
+                <a href="#" class="btn-floating"><i class="material-icons">content_copy</i></a>
+                <a @click="deleteTheme()" class="btn-floating"><i class="material-icons">delete</i></a>
+            </div>
         </div>
     </div>
 </template>
@@ -26,27 +33,39 @@
 
         props: ["theme"],
 
-        data: function() {
+        data: function () {
             return {
-                "isDeleted" : false,
-            }
+                "isDeleted": false,
+                "editing": false,
+            };
         },
 
         computed: {
-            sectionsLink: function() {
+            sectionsLink: function () {
                 return "/theme/" + this.id + "/sections";
             },
         },
 
         methods: {
-            sectionLink: function(section) {
+            sectionLink: function (section) {
                 return "/theme/" + this.theme.id + "/sections/" + section.id + "/fields";
             },
 
-            deleteTheme: function() {
+            deleteTheme: function () {
                 axios.delete('/theme/' + this.theme.id)
-                    .then( response => {
-                        if(response.data.success) this.isDeleted = true;
+                    .then(response => {
+                        if (response.data.success) this.isDeleted = true;
+                    });
+            },
+
+            edit: function () {
+                this.editing = true;
+            },
+
+            saveChanges: function () {
+                axios.put('/theme/' + this.theme.id, {name: this.theme.name})
+                    .then(response => {
+                        if (response.data.success) this.editing = false;
                     });
             },
         }
