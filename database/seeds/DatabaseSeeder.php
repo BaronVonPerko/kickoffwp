363e83d61e68b7acd637e6559369030fc05612e3
Chris Perko <Chris@chrisperko.net>
2017-11-14 18:16:50 -0600

    Removed theme seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          $this->call(UserSeeder::class);
-         $this->call(ThemeSeeder::class);
+//         $this->call(ThemeSeeder::class);
     }
 }
