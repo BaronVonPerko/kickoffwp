096a591dcecd2f39bb1664c8999380cb00ae6a01
Chris Perko <Chris@chrisperko.net>
2017-11-14 21:21:33 -0600

    Added save/edit buttons to field table

--- resources/assets/js/components/FieldListItem.vue
+++ resources/assets/js/components/FieldListItem.vue
@@ -1,10 +1,23 @@
 <template>
     <tr>
-        <td>{{field.label}}</td>
+        <td>
+            <span v-if="!isEditing">{{field.label}}</span>
+            <div class="field" v-if="isEditing">
+                <div class="control">
+                    <input placeholder="Field Label" id="label" name="label" class="input" v-model="field.label" required>
+                </div>
+            </div>
+        </td>
         <td>{{field.default || 'null'}}</td>
         <td>
             <a v-if="!isDeleting && !isEditing" @click="edit()" class="is-primary">
                 <i class="material-icons">edit</i>
+            </a>
+            <a v-if="isEditing" @click="save()" class="button is-primary">
+                Save
+            </a>
+            <a v-if="isEditing" @click="cancel()" class="button">
+                Cancel
             </a>
         </td>
     </tr>
@@ -19,12 +32,20 @@
                 isDeleting: false,
                 isDeleted: false,
                 isEditing: false,
+                originalLabel: this.field.label,
             }
         },
 
         methods: {
             edit: function() {
                 this.isEditing = true;
+            },
+            save: function() {
+                this.isEditing = false;
+            },
+            cancel: function() {
+                this.field.label = this.originalLabel;
+                this.isEditing = false;
             },
         },
     };
