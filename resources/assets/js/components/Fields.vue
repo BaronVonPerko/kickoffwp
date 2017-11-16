7713dea32f9e37d521a8c667a8cf1a0c6d8ec577
Chris Perko <Chris@chrisperko.net>
2017-11-15 22:19:29 -0600

    Changed layout of fields page

--- resources/assets/js/components/Fields.vue
+++ resources/assets/js/components/Fields.vue
@@ -14,21 +14,22 @@
                                 Create the fields you would like to display
                                 within this Customizer's section.
                             </p>
+
+                            <create-field-form :section-id="sectionId"
+                                               :theme-id="themeId"
+                                               :newFieldCreated="newFieldCreated">
+                            </create-field-form>
                         </div>
                     </div>
                 </div>
 
                 <div class="column">
-                    <create-field-form :section-id="sectionId"
-                                       :theme-id="themeId"
-                                       :newFieldCreated="newFieldCreated">
-                    </create-field-form>
+                    <field-list :fields="fieldList" :theme-id="themeId" :section-id="sectionId"></field-list>
                 </div>
 
             </div>
         </div>
 
-        <field-list :fields="fieldList" :theme-id="themeId" :section-id="sectionId"></field-list>
     </div>
 </template>
 
