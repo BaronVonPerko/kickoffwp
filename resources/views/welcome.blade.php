161acc33a630b750a1794f226f17a624da6bd4c0
Chris Perko <Chris@chrisperko.net>
2017-11-15 21:20:11 -0600

    Fixed sizing of welcome page on large monitors

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,18 +5,20 @@
     <div class="container-fluid is-light is-full-height hero-pattern">
         <section class="hero">
             <div class="hero-body">
-                <div class="columns is-tablet">
-                    <div class="column is-7">
-                        <img class="responsive-img" src="{{asset('images/code-screenshot.png')}}" alt="">
-                    </div>
-                    <div class="column is-5">
-                        <h1 class="title">Kick off your WordPress customizer files.</h1>
-                        <h2 class="subtitle">
-                            Building customizer classes is tedious and takes time KickoffWP does it for
-                            you quickly and easily.
-                        </h2>
+                <div class="container">
+                    <div class="columns is-tablet">
+                        <div class="column is-7">
+                            <img class="responsive-img" src="{{asset('images/code-screenshot.png')}}" alt="">
+                        </div>
+                        <div class="column is-5">
+                            <h1 class="title">Kick off your WordPress customizer files.</h1>
+                            <h2 class="subtitle">
+                                Building customizer classes is tedious and takes time KickoffWP does it for
+                                you quickly and easily.
+                            </h2>
 
-                        <welcome-email-signup></welcome-email-signup>
+                            <welcome-email-signup></welcome-email-signup>
+                        </div>
                     </div>
                 </div>
             </div>
