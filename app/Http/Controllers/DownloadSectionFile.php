3caa60e0849893cd29d506aed82e75c53028d7ec
Chris Perko <Chris@chrisperko.net>
2017-11-26 13:43:26 -0600

    Progress on section file generator

--- app/Http/Controllers/DownloadSectionFile.php
+++ app/Http/Controllers/DownloadSectionFile.php
@@ -24,10 +24,22 @@ class DownloadSectionFile extends Controller
     private $theme;
 
     /**
+     * Theme Name, without spaces
+     * @var string
+     */
+    private $themeName;
+
+    /**
      * The section to download.
      * @var \App\Section
      */
     private $section;
+
+    /**
+     * Section Name, without spaces
+     * @var string
+     */
+    private $sectionName;
 
     /**
      * Collection of fields on the given section.
@@ -46,14 +58,20 @@ class DownloadSectionFile extends Controller
 
         $this->getObjects($themeId, $sectionId);
 
-        $filename = str_replace(" ", "", $this->section->name) . ".php";
+        $filename = "$this->sectionName.php";
 
-        Storage::disk('public')->put($filename, 'hello world');
+        $contents = $this->files->get(__DIR__ . '/../../Stubs/SectionCustomizer.stub');
+
+        $contents = $this->setupFile($contents);
+        $contents = $this->setupSettings($contents);
+        $contents = $this->setupControls($contents);
+
+        dd($contents);
+
+        Storage::disk('public')->put($filename, $contents);
+
         return response()->download("storage/$filename")
                          ->deleteFileAfterSend(true);
-
-//        $this->files->put("public/files/test.txt", "hello world");
-//        return response()->download("images/code-screenshot.png");
     }
 
     /**
@@ -67,5 +85,71 @@ class DownloadSectionFile extends Controller
         $this->theme   = Theme::find($themeId);
         $this->section = Section::find($sectionId);
         $this->fields  = $this->section->fields;
+
+        $this->themeName   = str_replace(' ', '', $this->theme->name);
+        $this->sectionName = str_replace(' ', '', $this->section->name);
+    }
+
+    private function setupFile($contents)
+    {
+        $contents = $this->replaceCommon($contents);
+
+        return $contents;
+    }
+
+    /**
+     * Generate the code to register each Customizer Field as a WP Setting.
+     *
+     * @param $contents
+     */
+    private function setupSettings($contents)
+    {
+        $fieldContents = '';
+
+        foreach ($this->fields as $field) {
+            $stub = $this->files->get(__DIR__ . '/../../Stubs/Setting.stub');
+            $stub = $this->replaceCommon($stub);
+
+            $stub = str_replace('~Default~', "'default' => '$field->default'", $stub) . "\n\n";
+
+            $fieldContents .= $stub;
+        }
+
+        return str_replace("~Settings~", $fieldContents, $contents);
+    }
+
+    /**
+     * Setup the customizer controls.
+     *
+     * @param $contents
+     */
+    private function setupControls($contents)
+    {
+        $controlContents = '';
+
+        foreach ($this->fields as $field) {
+            $stub = $this->files->get(__DIR__ . '/../../Stubs/Control.stub');
+            $stub = str_replace('~FieldName~', str_replace(' ', '', $field->label), $stub);
+            $stub = str_replace('~FieldNameFull~', $field->label, $stub);
+            $stub = $this->replaceCommon($stub);
+
+            $controlContents .= $stub . "\n\n";
+        }
+
+        return str_replace("~Controls~", $controlContents, $contents);
+    }
+
+    /**
+     * Replace the common elements in the given stub contents.
+     *
+     * @param $contents
+     */
+    private function replaceCommon($contents)
+    {
+        $contents = str_replace('~ThemeName~', $this->themeName, $contents);
+        $contents = str_replace('~SectionName~', $this->sectionName, $contents);
+        $contents = str_replace('~SectionNameFull~', $this->section->name, $contents);
+
+        return $contents;
     }
 }
