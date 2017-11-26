c7d97a2b6f87cc0667ddaa78b9f1c37a6f1e0ca1
Chris Perko <Chris@chrisperko.net>
2017-11-26 11:44:28 -0600

    Run nightly report at the end of the UTC day

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->job(new NightlyReport)->dailyAt('23:00');
+         $schedule->job(new NightlyReport)->dailyAt('23:59');
     }
 
     /**
