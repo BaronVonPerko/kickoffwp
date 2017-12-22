655a16dd5440c863c8cafc1d6049eed3a7ce335b
Chris Perko <Chris@chrisperko.net>
2017-12-21 20:55:14 -0600

    Refactored navbar into its own file

---
+++ resources/views/parts/navbar.blade.php
@@ -0,0 +1,45 @@
+<nav class="navbar is-primary">
+    <div class="container">
+        <div class="navbar-brand">
+            <a href="/" class="navbar-item is-size-4">KickoffWP</a>
+
+            <button class="button navbar-burger">
+                <span></span><span></span><span></span>
+            </button>
+        </div>
+
+        @if(env('APP_ENV') == 'local')
+            <div class="navbar-menu">
+                <div class="navbar-start">
+                    <a class="navbar-item" target="_blank"
+                       href="https://github.com/BaronVonPerko/kickoffwp">
+                        Contribute
+                    </a>
+
+                    <a class="navbar-item" target="_blank"
+                       href="https://github.com/BaronVonPerko/kickoffwp/issues">
+                        Report Issues
+                    </a>
+                </div>
+                <div class="navbar-end">
+
+                    @auth()
+                        <a href="/start" class="navbar-item">Themes</a>
+                    @endauth
+                    @guest()
+                        <a href="/start" class="navbar-item">Start</a>
+                    @endguest
+
+                    @guest()
+                        <a href="/login" class="navbar-item">Login</a>
+                        <a href="/register" class="navbar-item">Register</a>
+                    @endguest
+
+                    @auth()
+                        <a href="/logout" class="navbar-item">Logout</a>
+                    @endauth
+                </div>
+            </div>
+        @endif
+    </div>
+</nav>
