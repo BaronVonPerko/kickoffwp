2bb973d0ce97b2bb8f58dd18febff685c399863b
Chris Perko <Chris@chrisperko.net>
2017-12-23 15:56:01 -0600

    sections in how to

--- resources/views/howto.blade.php
+++ resources/views/howto.blade.php
@@ -36,4 +36,39 @@
         </div>
     </div>
 
+    <div class="section">
+        <div class="container">
+            <h3 class="title">Sections</h3>
+            <div class="columns">
+                <div class="column is-one-quarter">
+                    <img src="{{asset('images/sections.png')}}" alt="WordPress Sections">
+                </div>
+                <div class="column">
+                    <p>
+                        Sections in KickoffWP correspond directly to sections in you WordPress
+                        administration panel.
+                    </p>
+                    <br>
+                    <p>
+                        Each section that you create in KickoffWP will create a generated PHP
+                        file for you to use in your WordPress Theme. These files are separated
+                        so that in the case that you need to modify the PHP code yourself, it
+                        is easy to find which file to modify.
+                    </p>
+                    <br>
+                    <p>
+                        It is very easy to register your section with your theme. All you need is
+                        to import the file into your <strong>functions.php</strong> file and call
+                        the static <em>Register</em> function, passing it the <strong>$wp_customize</strong>
+                        variable provide by WordPress.
+                    </p>
+                    <figure class="image howto-image">
+                        <img src="{{asset('images/functions-example.png')}}"
+                             alt="Example Registration">
+                    </figure>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
